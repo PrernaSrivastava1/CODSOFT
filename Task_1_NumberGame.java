aa067bb56286d46767b786a6a40a0f8d08fdfaff
PrernaSrivastava1 <prerna7105@gmail.com>
2023-11-23 23:09:21 +0530

    add variables.

---
+++ Task_1_NumberGame.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+
+ class Task_1_NumberGame{
+
+   public static void main(String[] args){
+       
+       int number;
+       int round=3;
+       int guess=1;
+       int attempts=10;
+       int score=0;
+        
+       
+
+       
+      
+
+
+    }
+ }
