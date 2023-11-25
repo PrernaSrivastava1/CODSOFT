42209ca448e53976bcd01e14c4ef6f28db5499ac
PrernaSrivastava1 <prerna7105@gmail.com>
2023-11-25 21:33:05 +0530

    add input

--- Task_2_StudentGrade_Calculator.java
+++ Task_2_StudentGrade_Calculator.java
@@ -1,6 +1,30 @@
 import java.util.*;
 
 public class Task_2_StudentGrade_Calculator {
-    
+
+    //introdunction.
+System.out.println(" -Student Grade Calculator- ");
+        
+//code to input marks of 5 subjects.
+System.out.println("Enter the Marks obtained in the respective 5 subjects");
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the marks Obtained out of 100 in Maths : ");
+int marksMaths = sc.nextInt();
+
+System.out.println("Enter the marks Obtained out of 100 in Physics: ");
+int marksPhy = sc.nextInt();
+
+System.out.println("Enter the marks Obtained out of 100 in Chemistry : ");
+int marksChem = sc.nextInt();
+
+System.out.println("Enter the marks Obtained out of 100 in Hindi : ");
+int marksHindi = sc.nextInt();
+
+System.out.println("Enter the marks Obtained out of 100 in English : ");
+int marksEng = sc.nextInt();
+
+
+
 
 }
