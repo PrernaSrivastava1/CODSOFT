ca8227f4f956f355d02717212bb5fb7bbe3a4539
PrernaSrivastava1 <prerna7105@gmail.com>
2023-11-25 23:56:33 +0530

    add result

--- Task_2_GradeCal.java
+++ Task_2_GradeCal.java
@@ -27,8 +27,51 @@ int marksHindi = sc.nextInt();
 System.out.println("Enter the marks Obtained out of 100 in English : ");
 int marksEng = sc.nextInt();
 
+//code for average percentage.
+int averagePer ;
+averagePer= (marksChem+marksEng+marksHindi+marksMaths+marksPhy)/5;
+
+int totalMarks=marksChem+marksEng+marksHindi+marksMaths+marksPhy;
+
+//code for grade assignment.
+if(averagePer>=90){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println(" You are having 'A+' Grades with the Average Percentage of :" +averagePer );
+}
+else if(averagePer < 90 && averagePer >= 80){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println("You are having 'A' Grades with the Average Percentage of :" +averagePer );
+}
+else if(averagePer < 80 && averagePer >= 70){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println("You are having 'B+' Grades with the Average Percentage of :" +averagePer );
+}
+else if(averagePer < 70 && averagePer >= 60){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println("You are having 'B' Grades with the Average Percentage of :" +averagePer );
+}
+else if(averagePer < 60 && averagePer >= 50){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println("You are having 'C' Grades with the Average Percentage of :" +averagePer );
+}
+else if(averagePer < 50 && averagePer >= 40){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println("You are having 'D' Grades with the Average Percentage of :" +averagePer );
+}
+else if (averagePer < 40 && averagePer >= 30){
+    System.out.println("Your Total Marks are :" +totalMarks );
+    System.out.println("You are having 'E' Grades with the Average Percentage of :" +averagePer );
+}
+ else if (averagePer < 30){
+   System.out.println("Your Total Marks are :" +totalMarks );
+   System.out.println("You are 'Fail!' with the Average Percentage of :" +averagePer );
+ }
+
 
 
     }
-
 }
+
+
+  
+
